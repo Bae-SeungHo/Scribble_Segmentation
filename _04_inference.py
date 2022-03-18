69af61ad2f550a7af908047004d748cf0f918d3e
Bae Seung-Ho <qold0blop@gmail.com>
2022-03-18 10:35:48 +0900

    bug fixed

--- _04_inference.py
+++ _04_inference.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description="inference model")
 
     parser.add_argument("-model", help="model dir", required=False,default=None)
-    parser.add_argument("-dataset", help="datasets dir", required=False , default='input/pascal/JPEGImages/')
+    parser.add_argument("-dataset", help="datasets dir", required=True )
     parser.add_argument("-output", help="output dir", required=False , default='output/')
     parser.add_argument("-clear", help="whether clear the output directory",action='store_true', default=False)
     parser.add_argument("-n_class", help="number of classes",required=False, default=None)
@@ -41,7 +41,8 @@ def main():
     
     #start inference
     os.system('rloss/v-env/bin/python3.7 rloss/pytorch/pytorch-deeplab_v3_plus/inference.py --backbone mobilenet --checkpoint %s --image_path %s --output_directory %s --n_class %d' % (model , args.dataset , args.output,n_class))
-
+    if os.path.isfile('temp.txt'):
+        os.remove('temp.txt')
     
 if __name__ == '__main__':
     main()
